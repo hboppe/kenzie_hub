08eebc3fbd66fcf9233e7159bb35fa4ea10bfc7d
hboppe <hanna.nascimento31@gmail.com>
2023-02-12 13:57:58 -0500

    fix: validation for LoginFormSchema

--- src/validations/LoginFormSchema.js
+++ src/validations/LoginFormSchema.js
@@ -3,10 +3,7 @@ import * as yup from 'yup';
 const LoginFormSchema = yup.object({
     email: yup.string().email('Digite um email válido').required('Email é obrigatório'),
     password: yup.string().required('Senha é obrigatória')
-        .matches(/[a-zA-Z]/, 'Deve conter pelo menos 1 letra minuscula e maiúscula')
-        .matches(/[\d]/, 'Deve conter pelo menos 1 número')
-        .matches(/[\W|_]/, 'Deve conter pelo menos 1 caracter especial')
-        .matches(/.{8,30}/, 'Deve ter no mínimo 8 e no máximo 30 caracteres')
+
 })
 
 
